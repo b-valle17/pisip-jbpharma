2bef351849d1bdbe0dfb3e2698dc003c459f17f2
b-valle17 <bryanvalle1217@gmail.com>
2026-06-28 14:13:21 -0500

    Fix: Eliminado JpaRepositorio duplicado en minúscula iUsuariojpaRepositorio

--- src/main/java/com/pisip/jbpharma/infraestructura/repositorio/iUsuariojpaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.pisip.jbpharma.infraestructura.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pisip.jbpharma.infraestructura.persistencia.jpa.UsuarioEntity;
-
-public interface iUsuariojpaRepositorio extends JpaRepository<UsuarioEntity, Integer> {
-
-}
