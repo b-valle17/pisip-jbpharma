833966e48daab70c33cda7cf2f023be010ac9d50
b-valle17 <bryanvalle1217@gmail.com>
2026-06-28 22:29:47 -0500

    feat: Avances en sprint2-produccion y solucion de conflictos

---
+++ src/main/java/com/pisip/jbpharma/presentacion/dto/request/UsuarioRequestDTO.java
@@ -0,0 +1,25 @@
+package com.pisip.jbpharma.presentacion.dto.request;
+
+import java.util.Date;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class UsuarioRequestDTO {
+	
+	@NotNull
+	private int idUsuario;
+	@NotBlank
+    private String nombre;
+	@NotBlank
+    private String correo;
+	@NotBlank
+    private String contrasenaHash;
+	@NotBlank
+    private boolean estadoUsuario;
+	@NotBlank
+    private Date fechaCreacion;
+
+}
