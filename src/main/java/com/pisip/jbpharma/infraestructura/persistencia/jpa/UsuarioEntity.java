9a5eff8a6a252c5192fc4a8e16ccbf5c9cc04304
kevin20202003 <kchala900@gmail.com>
2026-07-11 11:40:51 -0500

    Add relationships between entities: ParametroCalidadEntity, ProductoEntity, RolEntity, and UsuarioEntity

--- src/main/java/com/pisip/jbpharma/infraestructura/persistencia/jpa/UsuarioEntity.java
+++ src/main/java/com/pisip/jbpharma/infraestructura/persistencia/jpa/UsuarioEntity.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -25,5 +27,9 @@ public class UsuarioEntity {
     private String contrasenaHash;
     private boolean estadoUsuario;
     private Date fechaCreacion;
+
+	@ManyToOne
+	@JoinColumn(name = "fk_rol")
+	private RolEntity fkRolEntity;
 
 }
