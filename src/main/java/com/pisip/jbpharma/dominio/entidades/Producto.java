9bab988fb2b29ed26bde986aaa0aa4a0e4f6f8bc
kevin20202003 <kchala900@gmail.com>
2026-07-21 17:48:20 -0500

    avance del sprint 1

--- src/main/java/com/pisip/jbpharma/dominio/entidades/Producto.java
+++ src/main/java/com/pisip/jbpharma/dominio/entidades/Producto.java
@@ -2,43 +2,42 @@ package com.pisip.jbpharma.dominio.entidades;
 
 public class Producto {
 
-	private int idProducto;
+	private Integer idProducto;
     private String nombreProducto;
     private String descripcion;
     
 	public Producto() {
 		super();
 	}
-	
-	public Producto(int idProducto, String nombreProducto, String descripcion) {
+
+	public Producto(Integer idProducto, String nombreProducto, String descripcion) {
 		super();
 		this.idProducto = idProducto;
 		this.nombreProducto = nombreProducto;
 		this.descripcion = descripcion;
 	}
-	
-	public int getIdProducto() {
+
+	public Integer getIdProducto() {
 		return idProducto;
 	}
-	
-	public void setIdProducto(int idProducto) {
+
+	public void setIdProducto(Integer idProducto) {
 		this.idProducto = idProducto;
 	}
-	
+
 	public String getNombreProducto() {
 		return nombreProducto;
 	}
-	
+
 	public void setNombreProducto(String nombreProducto) {
 		this.nombreProducto = nombreProducto;
 	}
-	
+
 	public String getDescripcion() {
 		return descripcion;
 	}
-	
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-    
 }
